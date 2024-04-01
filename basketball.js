a30068d2fb87847e636e64954a49401cc9053e92
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-04-01 17:17:07 -0400

    basketball and basket on page

--- basketball.js
+++ basketball.js
@@ -1,4 +1,4 @@
 function bringToTop(id) {
     const div = document.getElementById(id);
-    div.style.zIndex = zIndex + 1; // Bring the selected div to the top
+    div.style.zIndex = id.zIndexz + 1; // Bring the selected div to the top
 }
