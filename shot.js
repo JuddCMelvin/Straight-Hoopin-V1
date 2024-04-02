fc26b63fff7b3b44b6cefb7a619ce735f57ad2d6
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-04-02 19:40:27 -0400

    hand is moving right to left

--- shot.js
+++ shot.js
@@ -7,21 +7,33 @@ let screenHeight = 600;
 let handX = screenWidth/2 - handWidth/2; 
 let handY = screenHeight - handheight - 10; 
 
-
+//Set Starting Position
 hand.style.left = handX + "px"; 
 hand.style.top = handY + "px";
-// function moveBasket(direction) {
-//     const stepBack = 10; 
 
-//     if (direction === 'left') {
-//         basketX -= stepBack;
-//     } else if (direction === 'right') {
-//         basketX += stepBack;
-//     }
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        // moveHand('left');
+        moveHand('left')
+        console.log('hello')
+    } else if (event.key === 'ArrowRight') {
+        // moveHand('right');
+        moveHand('right')
+        console.log('')
+    }
+});
 
-//     basket.style.left = basketX + 'px';
+function moveHand(direction) {
+    let dribble = 10; 
 
-// }
+    if (direction === 'left') {
+        handX -= dribble;
+        hand.style.left = handX + 'px';
+    } else if (direction === 'right') {
+        handX += dribble;
+        hand.style.left = handX + 'px';
+    }
+}
 
 // function moveBasket() {
 //     handX += direction;
